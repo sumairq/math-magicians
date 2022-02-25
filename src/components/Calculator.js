23f64115e7c38caf0c21203e90713487c5c1e6ac
Sumair <sumairqaisar992@gmail.com>
2022-02-26 03:26:25 +0500

    Refactor with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,56 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const stateObject = {
+    total: 0,
+    next: null,
+    operation: null,
+  };
 
-  handleClick(btn) {
-    const txt = btn.target.textContent;
-    this.setState((e) => calculate(e, txt));
-  }
+  const [property, setProperty] = useState(stateObject);
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="container">
-        <div className="result">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="buttons">
-          <button type="button" onClick={this.handleClick} className="button resetBtn">AC</button>
-          <button type="button" onClick={this.handleClick} className="button negative operator">+/-</button>
-          <button type="button" onClick={this.handleClick} className="button operator percent">%</button>
-          <button type="button" onClick={this.handleClick} className="button operator">/</button>
-          <button type="button" onClick={this.handleClick} className="button number">7</button>
-          <button type="button" onClick={this.handleClick} className="button number">8</button>
-          <button type="button" onClick={this.handleClick} className="button number">9</button>
-          <button type="button" onClick={this.handleClick} className="button operator">*</button>
-          <button type="button" onClick={this.handleClick} className="button number">4</button>
-          <button type="button" onClick={this.handleClick} className="button number">5</button>
-          <button type="button" onClick={this.handleClick} className="button number">6</button>
-          <button type="button" onClick={this.handleClick} className="button operator">-</button>
-          <button type="button" onClick={this.handleClick} className="button number">1</button>
-          <button type="button" onClick={this.handleClick} className="button number">2</button>
-          <button type="button" onClick={this.handleClick} className="button number">3</button>
-          <button type="button" onClick={this.handleClick} className="button operator">+</button>
-          <button type="button" onClick={this.handleClick} className="button number zero-btn">0</button>
-          <button type="button" onClick={this.handleClick} className="button number dot">.</button>
-          <button type="button" onClick={this.handleClick} className="button  operator">=</button>
-        </div>
+  const handleClick = (btn) => {
+    const funcBtn = btn.target.textContent;
+    const newProperty = { ...property };
+    setProperty(calculate(newProperty, funcBtn));
+  };
+
+  return (
+    <div className="container">
+      <div className="result">
+        {property.total}
+        {property.operation}
+        {property.next}
       </div>
-    );
-  }
-}
+      <div className="buttons">
+        <button type="button" onClick={handleClick} className="button resetBtn">AC</button>
+        <button type="button" onClick={handleClick} className="button negative operator">+/-</button>
+        <button type="button" onClick={handleClick} className="button operator percent">%</button>
+        <button type="button" onClick={handleClick} className="button operator">/</button>
+        <button type="button" onClick={handleClick} className="button number">7</button>
+        <button type="button" onClick={handleClick} className="button number">8</button>
+        <button type="button" onClick={handleClick} className="button number">9</button>
+        <button type="button" onClick={handleClick} className="button operator">*</button>
+        <button type="button" onClick={handleClick} className="button number">4</button>
+        <button type="button" onClick={handleClick} className="button number">5</button>
+        <button type="button" onClick={handleClick} className="button number">6</button>
+        <button type="button" onClick={handleClick} className="button operator">-</button>
+        <button type="button" onClick={handleClick} className="button number">1</button>
+        <button type="button" onClick={handleClick} className="button number">2</button>
+        <button type="button" onClick={handleClick} className="button number">3</button>
+        <button type="button" onClick={handleClick} className="button operator">+</button>
+        <button type="button" onClick={handleClick} className="button number zero-btn">0</button>
+        <button type="button" onClick={handleClick} className="button number dot">.</button>
+        <button type="button" onClick={handleClick} className="button  operator">=</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
